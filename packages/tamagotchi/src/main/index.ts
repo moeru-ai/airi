a0db6bce7bc25298b17eb4ddb7268cc563f6634e
LemonNeko <chheese048@gmail.com>
2025-01-03 00:01:08 +0800

    remove duplicate icon

--- packages/tamagotchi/src/main/index.ts
+++ packages/tamagotchi/src/main/index.ts
@@ -3,7 +3,7 @@ import { join } from 'node:path'
 import process from 'node:process'
 import { electronApp, is, optimizer } from '@electron-toolkit/utils'
 import { app, BrowserWindow, ipcMain, shell } from 'electron'
-import icon from '../../resources/icon.png?asset'
+import icon from '../../build/icon.png?asset'
 
 function createWindow(): void {
   // Create the browser window.
