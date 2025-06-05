d9a5b421f1c93ba604d742c909dad6cb4e04c5d8
LJ5O <75009579+LJ5O@users.noreply.github.com>
2025-06-05 17:14:20 +0200

    Display layout and input component

---
+++ apps/stage-web/src/pages/settings/airi-card/components/moveMeLaterInput.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+interface Props {
+  label: string
+  long?: boolean
+  required?: boolean
+  placeholder?: string
+  isActive?: boolean
+}
+
+withDefaults(defineProps<Props>(), {
+  long: false,
+  placeholder: 'Type something here...',
+  isActive: true,
+  required: false,
+})
+
+// value is a Ref
+const value = defineModel<string>() // Linked to v-model when this element is placed somewhere
+</script>
+
+<template>
+  <div class="flex flex-col gap-1">
+    <label>{{ label }}</label>
+    <textarea
+      v-if="long"
+      v-model="value" type="string"
+      class="rounded-xl p-2.5 text-sm outline-none"
+      :class="value.length !== 0 || !required ? 'bg-white dark:bg-neutral-900' : 'bg-red-900'"
+      border="focus:primary-100 dark:focus:primary-400/50 2 solid neutral-200 dark:neutral-800"
+      transition="all duration-200 ease-in-out"
+    />
+
+    <input
+      v-else
+      v-model="value" type="string"
+      class="rounded-xl p-2.5 text-sm outline-none"
+      :class="value.length !== 0 || !required ? 'bg-white dark:bg-neutral-900' : 'bg-red-900'"
+      border="focus:primary-100 dark:focus:primary-400/50 2 solid neutral-200 dark:neutral-800"
+      transition="all duration-200 ease-in-out"
+    >
+  </div>
+</template>
