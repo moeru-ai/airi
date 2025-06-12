f58e6bf24c30de666ac0780e9e35eab32ce4b97a
Menci <mencici@msn.com>
2025-06-12 23:21:46 +0800

    feat(ui): transition for loading / icon button

--- packages/stage-ui/src/components/Misc/index.ts
+++ packages/stage-ui/src/components/Misc/index.ts
@@ -1,3 +1,4 @@
 export { default as Collapsable } from './Collapsable.vue'
 export { default as Skeleton } from './Skeleton.vue'
+export { default as BidirectionalTransition } from './BidirectionalTransition.vue'
 export * from './Steppers'
