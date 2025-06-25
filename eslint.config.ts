2805421e915e88230011ea8040493256aa6b73c0
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-06-25 15:57:58 +0800

    perf(eslint): use moeru config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,41 +1,44 @@
-import antfu from '@antfu/eslint-config'
+import { defineConfig } from '@moeru/eslint-config'
 
-export default await antfu(
-  {
-    unocss: true,
-    vue: true,
-    ignores: [
-      '**/assets/js/**',
-      '**/assets/live2d/models/**',
-      'apps/stage-tamagotchi/out/**',
-      'apps/stage-tamagotchi/src-tauri/**',
-      'crates/**',
-      '**/drizzle/**',
-      '**/.astro/**',
+export default defineConfig({
+  oxlint: true,
+  preferLet: false,
+  typescript: true,
+  unocss: true,
+  vue: true,
+}, {
+  ignores: [
+    '**/assets/js/**',
+    '**/assets/live2d/models/**',
+    'apps/stage-tamagotchi/out/**',
+    'apps/stage-tamagotchi/src-tauri/**',
+    'crates/**',
+    '**/drizzle/**',
+    '**/.astro/**',
+  ],
+}, {
+  rules: {
+    'antfu/import-dedupe': 'error',
+    'import/order': 'off',
+    'perfectionist/sort-imports': [
+      'error',
+      {
+        groups: [
+          'type-builtin',
+          'type-import',
+          'type-internal',
+          ['type-parent', 'type-sibling', 'type-index'],
+          'value-builtin',
+          'value-external',
+          'value-internal',
+          ['value-parent', 'value-sibling', 'value-index'],
+          ['wildcard-value-parent', 'wildcard-value-sibling', 'wildcard-value-index'],
+          'side-effect',
+          'style',
+        ],
+        newlinesBetween: 'always',
+      },
     ],
-    rules: {
-      'import/order': 'off',
-      'antfu/import-dedupe': 'error',
-      'style/padding-line-between-statements': 'error',
-      'perfectionist/sort-imports': [
-        'error',
-        {
-          groups: [
-            'type-builtin',
-            'type-import',
-            'type-internal',
-            ['type-parent', 'type-sibling', 'type-index'],
-            'value-builtin',
-            'value-external',
-            'value-internal',
-            ['value-parent', 'value-sibling', 'value-index'],
-            ['wildcard-value-parent', 'wildcard-value-sibling', 'wildcard-value-index'],
-            'side-effect',
-            'style',
-          ],
-          newlinesBetween: 'always',
-        },
-      ],
-    },
+    'style/padding-line-between-statements': 'error',
   },
-)
+})
