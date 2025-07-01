53c6ed8f0e39fd7c2273d0b9b05c94bd8741366a
lorisj <lorisjautakas@gmail.com>
2025-07-01 11:59:12 -0400

    add player2-speech to providers

--- packages/stage-ui/src/stores/providers.ts
+++ packages/stage-ui/src/stores/providers.ts
@@ -1218,6 +1218,79 @@ export const useProvidersStore = defineStore('providers', () => {
         },
       },
     },
+    'player2-speech': {
+      id: 'player2-speech',
+      category: 'speech',
+      tasks: ['text-to-speech'],
+      nameKey: 'settings.pages.providers.provider.player2.title',
+      name: 'Player2 Speech',
+      descriptionKey: 'settings.pages.providers.provider.player2.description',
+      description: 'player2.game',
+      icon: 'i-lobe-icons:player2',
+      defaultOptions: () => ({
+        baseUrl: 'http://localhost:4315/v1/',
+      }),
+      createProvider: config => createPlayer2((config.baseUrl as string).trim(), 'airi'),
+      capabilities: {
+        listVoices: async () => {
+          return await fetch('http://localhost:4315/v1/tts/voices').then(res => res.json()).then(({ voices }) => (voices as { id: string, language: 'american_english' | 'british_english' | 'japanese' | 'mandarin_chinese' | 'spanish' | 'french' | 'hindi' | 'italian' | 'brazilian_portuguese', name: string, gender: string }[]).map(({ id, language, name, gender }) => (
+            {
+
+              id,
+              name,
+              provider: 'player2-speech',
+              gender,
+              languages: [{
+                american_english: {
+                  code: 'en',
+                  title: 'English',
+                },
+                british_english: {
+                  code: 'en',
+                  title: 'English',
+                },
+                japanese: {
+                  code: 'ja',
+                  title: 'Japanese',
+                },
+                mandarin_chinese: {
+                  code: 'zh',
+                  title: 'Chinese',
+                },
+                spanish: {
+                  code: 'es',
+                  title: 'Spanish',
+                },
+                french: {
+                  code: 'fr',
+                  title: 'French',
+                },
+                hindi: {
+                  code: 'hi',
+                  title: 'Hindi',
+                },
+
+                italian: {
+                  code: 'it',
+                  title: 'Italian',
+                },
+                brazilian_portuguese:
+                {
+                  code: 'pt',
+                  title: 'Portuguese',
+                },
+
+              }[language]],
+            }
+          )))
+        },
+      },
+      validators: {
+        validateProviderConfig: (config: any) => {
+          return !!config.baseUrl
+        },
+      },
+    },
     'cloudflare-workers-ai': {
       id: 'cloudflare-workers-ai',
       category: 'chat',
