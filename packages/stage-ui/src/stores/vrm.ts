a009cb9c5f0b2c934a1716bf796937c1df4249c0
Kapapu_Lilia <chenalenavenger@gmail.com>
2025-07-18 01:02:26 +0100

    add vrm model uploading functionality

--- packages/stage-ui/src/stores/vrm.ts
+++ packages/stage-ui/src/stores/vrm.ts
@@ -1,11 +1,16 @@
 import { useLocalStorage } from '@vueuse/core'
 import { defineStore } from 'pinia'
+<<<<<<< HEAD
 import { computed, ref, watch } from 'vue'
+=======
+import { computed, ref } from 'vue'
+>>>>>>> de20e991 (add vrm model uploading functionality)
 
 export const useVRM = defineStore('vrm', () => {
   const modelFile = ref<File>()
   const modelUrl = ref<string>('/assets/vrm/models/AvatarSample-B/AvatarSample_B.vrm')
   const loadSource = ref<'file' | 'url'>('url')
+<<<<<<< HEAD
   const loadingModel = ref(false)
   const modelSize = useLocalStorage('settings/vrm/modelSize', { x: 0, y: 0, z: 0 })
   const modelOrigin = useLocalStorage('settings/vrm/modelOrigin', { x: 0, y: 0, z: 0 })
@@ -46,16 +51,30 @@ export const useVRM = defineStore('vrm', () => {
     return '/assets/vrm/models/AvatarSample-B/AvatarSample_B.vrm'
   })
 
+=======
+  const loadingModel = ref(false) // if set to true, the model will be loaded
+  const position = useLocalStorage('settings/live2d/position', { x: 0, y: 0 }) // position is relative to the center of the screen, units are %
+  const positionInPercentageString = computed(() => ({
+    x: `${position.value.x}%`,
+    y: `${position.value.y}%`,
+  }))
+
+>>>>>>> de20e991 (add vrm model uploading functionality)
   return {
     modelFile,
     modelUrl,
     loadSource,
     loadingModel,
+<<<<<<< HEAD
     modelSize,
     modelOrigin,
     modelOffset,
     modelPosition,
     positionInPercentageString,
     selectedModel, // Expose the new computed property
+=======
+    position,
+    positionInPercentageString,
+>>>>>>> de20e991 (add vrm model uploading functionality)
   }
 })
