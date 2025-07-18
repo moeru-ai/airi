168b970a2c700e94e94422132fd1e08c65f5bf17
Lilia_Chen <chenalenavenger@gmail.com>
2025-07-18 11:50:32 +0100

    Update packages/stage-ui/src/components/Scenarios/Settings/ModelSettings/VRMScene.vue
    Co-authored-by: Neko <neko@ayaka.moe>

--- packages/stage-ui/src/components/Scenarios/Settings/ModelSettings/VRMScene.vue
+++ packages/stage-ui/src/components/Scenarios/Settings/ModelSettings/VRMScene.vue
@@ -53,7 +53,6 @@ const selectedModel = computed(() => {
       <TresPerspectiveCamera :position="[cameraPositionX, cameraPositionY, cameraPositionZ]" />
       <TresDirectionalLight :color="0xFFFFFF" :intensity="1.2" :position="[1, 1, 1]" />
       <TresAmbientLight :color="0xFFFFFF" :intensity="1.5" />
-      <!-- vrma animation import still need to be done -->
       <VRMModel
         ref="modelRef"
         :key="selectedModel"
