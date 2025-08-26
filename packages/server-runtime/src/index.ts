1379415d464d97b6daacb075ffb6f009268fe1c5
Iro <155815508+Iro96@users.noreply.github.com>
2025-08-26 16:13:04 +0700

    fix lint

--- packages/server-runtime/src/index.ts
+++ packages/server-runtime/src/index.ts
@@ -99,19 +99,17 @@ function main() {
           const p = peers.get(peer.id)
           if (p) {
             unregisterModulePeer(p)
-        
             const { name, index } = event.data as { name: string; index?: number }
             if (!name || typeof name !== 'string') {
-              send(peer, { type: 'error', data: { message: "the field 'name' must be a non-empty string for event 'module:announce'" } })
+              send(peer, { type: 'error', data: { message: 'the field 'name' must be a non-empty string for event 'module:announce'' } })
               return
             }
             if (typeof index !== 'undefined') {
               if (typeof index !== 'number' || index < 0) {
-                send(peer, { type: 'error', data: { message: "the field 'index' must be a non-negative number for event 'module:announce'" } })
+                send(peer, { type: 'error', data: { message: 'the field 'index' must be a non-negative number for event 'module:announce'' } })
                 return
               }
             }
-        
             Object.assign(p, { authenticated: true, name, index })
             registerModulePeer(p, p.name, p.index)
           }
