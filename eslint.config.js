1fd4a8c01508d61856162796fabb91437a04cd55
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-10-18 17:48:50 +0800

    chore(eslint): use moeru config, disable lint-staged

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from '@moeru/eslint-config'
+
+export default defineConfig({
+  unocss: true,
+  vue: true,
+}, {
+  ignores: [
+    'cspell.config.yaml',
+    'cspell.config.yml',
+    'crowdin.yaml',
+    'crowdin.yml',
+    '**/assets/js/**',
+    '**/assets/live2d/models/**',
+    'apps/stage-tamagotchi/out/**',
+    'apps/stage-tamagotchi/src/bindings/**',
+    'apps/stage-tamagotchi/src-tauri/**',
+    'apps/stage-tamagotchi-electron/out/**',
+    'apps/stage-tamagotchi-electron/src/renderer/bindings/**',
+    'crates/**',
+    '**/drizzle/**',
+    '**/.astro/**',
+  ],
+}, {
+  rules: {
+    'antfu/import-dedupe': 'error',
+    // TODO: remove this
+    'depend/ban-dependencies': 'warn',
+    'import/order': 'off',
+    'no-console': ['error', { allow: ['warn', 'error', 'info'] }],
+    'style/padding-line-between-statements': 'error',
+    'vue/prefer-separate-static-class': 'off',
+    'yaml/plain-scalar': 'off',
+  },
+})
