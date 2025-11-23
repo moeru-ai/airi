f94ecb6114392d2cb5e621da556c168bc8f23425
Neko <neko@ayaka.moe>
2025-11-23 15:12:24 +0800

    Update packages/ui/src/composables/use-deferred-mount.ts

--- packages/ui/src/composables/use-deferred-mount.ts
+++ packages/ui/src/composables/use-deferred-mount.ts
@@ -4,6 +4,9 @@ export function useDeferredMount() {
   const isReady = ref(false)
 
   onMounted(() => {
+    // NOTICE: yield the main thread to the browser to paint.
+    // Other approaches work too, e.g. double RAF like here, or a setTimeout 0, or Suspense with async component, etc. 
+    // Virtualization somewhat helps, however, virtual list may break Ctrl-F and Ctrl-A.
     requestAnimationFrame(() => {
       requestAnimationFrame(() => {
         isReady.value = true
