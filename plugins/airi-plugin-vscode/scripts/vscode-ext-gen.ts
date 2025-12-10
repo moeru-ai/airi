e978566e44112c91521dc34b5ede008317d858e2
Neko Ayaka <neko@ayaka.moe>
2025-12-11 02:26:04 +0800

    fix(ci): cannot resolve to correct path

--- plugins/airi-plugin-vscode/scripts/vscode-ext-gen.ts
+++ plugins/airi-plugin-vscode/scripts/vscode-ext-gen.ts
@@ -1,5 +1,6 @@
 import { mkdir, rm, writeFile } from 'node:fs/promises'
 import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
 import { generate } from 'vscode-ext-gen'
 
@@ -8,15 +9,17 @@ import { packageJSONForVSCode } from './shared'
 async function run() {
   const { restore, json } = await packageJSONForVSCode('airi-vscode')
   const generated = generate(json, { extensionScope: 'airi-vscode' })
+  const url = fileURLToPath(new URL('.', import.meta.url))
+  const dir = dirname(url)
 
   try {
-    await rm(join(dirname(new URL('.', import.meta.url).pathname), 'src', 'generated'), { force: true })
+    await rm(join(dir, 'src', 'generated'), { force: true })
   }
   catch {
   }
 
-  await mkdir(join(dirname(new URL('.', import.meta.url).pathname), 'src', 'generated'), { recursive: true })
-  await writeFile(join(dirname(new URL('.', import.meta.url).pathname), 'src', 'generated', 'meta.ts'), generated.dts, 'utf-8')
+  await mkdir(join(dir, 'src', 'generated'), { recursive: true })
+  await writeFile(join(dir, 'src', 'generated', 'meta.ts'), generated.dts, 'utf-8')
   await restore()
 }
 
