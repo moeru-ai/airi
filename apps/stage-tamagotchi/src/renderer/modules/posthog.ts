e5d6f71d1b855d42cd2d614fbbcad5be3f3ac2fd
Neko Ayaka <neko@ayaka.moe>
2025-12-14 18:02:55 +0800

    chore(stage-tamagotchi): use another posthog id

--- apps/stage-tamagotchi/src/renderer/modules/posthog.ts
+++ apps/stage-tamagotchi/src/renderer/modules/posthog.ts
@@ -1,6 +1,6 @@
 import posthog from 'posthog-js'
 
-posthog.init('phc_pzjziJjrVZpa9SqnQqq0QEKvkmuCPH7GDTA6TbRTEf9', {
+posthog.init('phc_rljw376z5gt6vXJlc3sTr7hFbXodciY9THEQXIRnW53', {
   api_host: 'https://us.i.posthog.com',
   person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
 })
