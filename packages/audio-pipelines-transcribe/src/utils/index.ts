e2e661cc7ecab0d1fa3a3f45a6f48142930b4e1c
Neko Ayaka <neko@ayaka.moe>
2025-12-22 19:40:39 +0800

    chore(deps): bump dependencies

---
+++ packages/audio-pipelines-transcribe/src/utils/index.ts
@@ -0,0 +1,32 @@
+export async function mediaStreamFromAudioFile(file: File): Promise<{
+  cleanup: () => Promise<void>
+  stream: MediaStream
+}> {
+  const audioContext = new AudioContext()
+  const arrayBuffer = await file.arrayBuffer()
+  const audioBuffer = await audioContext.decodeAudioData(arrayBuffer)
+
+  const source = audioContext.createBufferSource()
+  source.buffer = audioBuffer
+
+  const destination = audioContext.createMediaStreamDestination()
+  source.connect(destination)
+  source.connect(audioContext.destination)
+
+  source.start(0)
+  await audioContext.resume()
+
+  return {
+    stream: destination.stream,
+    cleanup: async () => {
+      try {
+        source.stop()
+      }
+
+      catch { /* noop */ }
+      source.disconnect()
+      destination.disconnect()
+      await audioContext.close()
+    },
+  }
+}
