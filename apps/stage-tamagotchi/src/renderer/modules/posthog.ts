66d7af9525b0755953a35c5e7e33ed73308cd73b
Rin <shinohara-rin@users.noreply.github.com>
2025-12-26 23:32:05 +0800

    chore(stage-web,stage-tamagotchi): remove dead code

--- apps/stage-tamagotchi/src/renderer/modules/posthog.ts
+++ apps/stage-tamagotchi/src/renderer/modules/posthog.ts
@@ -4,5 +4,3 @@ posthog.init('phc_rljw376z5gt6vXJlc3sTr7hFbXodciY9THEQXIRnW53', {
   api_host: 'https://us.i.posthog.com',
   person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
 })
-
-export default posthog
--- apps/stage-web/src/modules/posthog.ts
+++ apps/stage-web/src/modules/posthog.ts
@@ -4,5 +4,3 @@ posthog.init('phc_pzjziJjrVZpa9SqnQqq0QEKvkmuCPH7GDTA6TbRTEf9', {
   api_host: 'https://us.i.posthog.com',
   person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
 })
-
-export default posthog
