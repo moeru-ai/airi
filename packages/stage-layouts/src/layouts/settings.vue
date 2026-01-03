7cb1c9459f973f3a7574643f9ea8c313e5fa406b
s3d-i <bing@sed-i.org>
2026-01-03 12:03:42 +0800

    chore: better naming
    refactor: responsive page, unified sizing
    
    chore: lag visualizer slug/i18n renaming

--- packages/stage-layouts/src/layouts/settings.vue
+++ packages/stage-layouts/src/layouts/settings.vue
@@ -110,9 +110,9 @@ const routeHeaderMetadataMap = computed(() => {
       subtitle: t('tamagotchi.settings.devtools.title'),
       title: t('tamagotchi.settings.devtools.pages.context-flow.title'),
     },
-    '/devtools/lag-visualizer': {
+    '/devtools/performance-visualizer': {
       subtitle: t('settings.title'),
-      title: t('settings.pages.system.sections.section.developer.sections.section.lag-visualizer.title'),
+      title: t('settings.pages.system.sections.section.developer.sections.section.performance-visualizer.title'),
     },
     '/devtools/markdown-stress': {
       subtitle: t('settings.title'),
