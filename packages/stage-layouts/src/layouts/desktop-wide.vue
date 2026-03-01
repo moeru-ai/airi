dea4c30f6c6cf79afe7dbbe6c9158b968925dc74
Neko Ayaka <neko@ayaka.moe>
2026-03-02 01:18:10 +0800

    feat(stage-tamagotchi): dashboard, character normalization

--- packages/stage-layouts/src/layouts/desktop-wide.vue
+++ packages/stage-layouts/src/layouts/desktop-wide.vue
@@ -1,15 +1,5 @@
 <script setup lang="ts">
-import { electron } from '@proj-airi/electron-eventa'
-import { useElectronEventaInvoke } from '@proj-airi/electron-vueuse'
-import { useAsyncState } from '@vueuse/core'
 import { RouterView } from 'vue-router'
-
-const isMacOSInvoke = useElectronEventaInvoke(electron.app.isMacOS)
-const isWindowsInvoke = useElectronEventaInvoke(electron.app.isWindows)
-const isLinuxInvoke = useElectronEventaInvoke(electron.app.isLinux)
-const { state: isMacOS } = useAsyncState(() => isMacOSInvoke(), false)
-const { state: isWindows } = useAsyncState(() => isWindowsInvoke(), false)
-const { state: isLinux } = useAsyncState(() => isLinuxInvoke(), false)
 </script>
 
 <template>
@@ -27,25 +17,12 @@ const { state: isLinux } = useAsyncState(() => isLinuxInvoke(), false)
   >
     <div
       :class="[
-        'mt-1 mx-1',
-        'flex flex-col items-center justify-center',
-        'w-[calc(100%-8px)]', 'py-2 px-2',
+        'h-[64px]',
         'drag-region',
-        'bg-black/35 dark:bg-white/35',
-        'text-white dark:text-black',
-        'backdrop-blur-xl',
-        'rounded-lg',
       ]"
-    >
-      <div v-if="isMacOS" class="" />
-      <div class="text-sm">
-        Project AIRI
-      </div>
-      <div v-if="isWindows || isLinux" class="" />
-    </div>
+    />
     <div
       :class="[
-        'mt-1',
         'overflow-y-auto',
       ]"
     >
