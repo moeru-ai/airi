5d8f9ce95f3a70f7f5d3cb0cfe3a81e12c1d8688
Makito <i@maki.to>
2026-03-12 00:31:53 +0900

    chore: adjust wording

--- apps/stage-tamagotchi/src/main/libs/i18n/index.ts
+++ apps/stage-tamagotchi/src/main/libs/i18n/index.ts
@@ -160,7 +160,7 @@ export function createI18n<Schema extends Record<string, any> = Record<string, a
     // ```
     // See: https://github.com/intlify/vue-i18n/blob/884060b63122ccca3088e4af283ef223a1e2bd29/packages/vue-i18n/src/runtime.ts#L15-L22
     //
-    // ... but we will provide them via `createCoreContext` for better scoping.
+    // ... but we will provide them to `createCoreContext` here for better scoping.
     messageCompiler: compile,
     messageResolver: resolveValue,
     localeFallbacker: fallbackWithLocaleChain,
