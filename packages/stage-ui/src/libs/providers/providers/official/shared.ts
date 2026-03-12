f50a260e8db5fece380ab6f9ccacec8bac926dbc
RainbowBird <git@luoling.moe>
2026-03-12 18:23:18 +0800

    refactor(stage-ui): unify official provider to plug-able, add auth lifecycle hooks

---
+++ packages/stage-ui/src/libs/providers/providers/official/shared.ts
@@ -0,0 +1,18 @@
+import { createOpenAI } from '@xsai-ext/providers/create'
+
+import { SERVER_URL } from '../../../../libs/server'
+
+export const OFFICIAL_ICON = 'i-solar:star-bold-duotone'
+
+export function withCredentials() {
+  return (input: RequestInfo | URL, init?: RequestInit) => {
+    return globalThis.fetch(input, {
+      ...init,
+      credentials: 'include',
+    })
+  }
+}
+
+export function createOfficialOpenAIProvider() {
+  return createOpenAI('', `${SERVER_URL}/api/v1/`)
+}
