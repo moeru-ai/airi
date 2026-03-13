69515e58c9bbf2f6a3e7409242bb6b4a3e3e36cd
RainbowBird <git@luoling.moe>
2026-03-13 23:18:22 +0800

    fix(stage-*): onboarding issues

--- packages/stage-layouts/src/layouts/stage.vue
+++ packages/stage-layouts/src/layouts/stage.vue
@@ -1,16 +1,23 @@
 <script setup lang="ts">
 import { LoginDrawer } from '@proj-airi/stage-ui/components/auth/index'
 import { useAuthStore } from '@proj-airi/stage-ui/stores/auth'
+import { useMediaQuery } from '@vueuse/core'
 import { storeToRefs } from 'pinia'
 import { RouterView } from 'vue-router'
 
-const authStore = useAuthStore()
-const { isLoginDrawerOpen } = storeToRefs(authStore)
+const isMobile = useMediaQuery('(max-width: 768px)')
+const { needsLogin } = storeToRefs(useAuthStore())
 </script>
 
 <template>
   <main h-full font-cute>
     <RouterView />
-    <LoginDrawer v-model:open="isLoginDrawerOpen" />
+
+    <Teleport to="body">
+      <LoginDrawer
+        v-if="isMobile"
+        v-model:open="needsLogin"
+      />
+    </Teleport>
   </main>
 </template>
