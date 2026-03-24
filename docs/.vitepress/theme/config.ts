144a7781b691f4670f504f90ad0ac94e2d121994
秋雨落 <i@rain.cx>
2026-03-24 12:48:56 +0800

    docs(docs): Add comment for target property.
    Signed-off-by: 秋雨落 <i@rain.cx>

--- docs/.vitepress/theme/config.ts
+++ docs/.vitepress/theme/config.ts
@@ -8,10 +8,22 @@ interface HomePageConfig {
   buttons: ButtonItem[]
 }
 
-export interface ButtonItem {
+export type ButtonItem = {
+  primary?: boolean
+} & Link
+
+export interface Link {
   text?: string
   link?: string
-  primary?: boolean
+
+  /**
+   * VitePress在SPA内部处理导航时会劫持<a>标签的点击事件，导致外部链接被Vue Router识别为内部路由，产生路由错误。<br/>
+   * 可以通过给<a>标签增加target属性让浏览器原生处理跳转的方式避免此问题。
+   *
+   * 参见：<br/>
+   * https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/a#target<br/>
+   * https://stackoverflow.com/questions/79348337/redirect-main-title-link-in-vitepress-to-my-personal-website/79386388#79386388
+   */
   target?: string
 }
 
