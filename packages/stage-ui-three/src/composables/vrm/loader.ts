44aec308a51d6c4d82f6bb843de2d253286069d2
Lilia-Chen <chenalenavenger@gmail.com>
2026-03-25 00:26:06 +0000

    fix(stage-ui-three): cutout bug fix

--- packages/stage-ui-three/src/composables/vrm/loader.ts
+++ packages/stage-ui-three/src/composables/vrm/loader.ts
@@ -1,4 +1,4 @@
-import { VRMLoaderPlugin } from '@pixiv/three-vrm'
+import { MToonMaterialLoaderPlugin, VRMLoaderPlugin } from '@pixiv/three-vrm'
 import { VRMAnimationLoaderPlugin } from '@pixiv/three-vrm-animation'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 
@@ -12,7 +12,15 @@ export function useVRMLoader() {
   loader = new GLTFLoader()
 
   loader.crossOrigin = 'anonymous'
-  loader.register(parser => new VRMLoaderPlugin(parser))
+  loader.register((parser) => {
+    const mtoonMaterialPlugin = new MToonMaterialLoaderPlugin(parser, {
+      v0CompatShade: true,
+    })
+
+    return new VRMLoaderPlugin(parser, {
+      mtoonMaterialPlugin,
+    })
+  })
   // loader.register(parser => new VRMCoreLoaderPlugin(parser))
   loader.register(parser => new VRMAnimationLoaderPlugin(parser))
 
