f65a67f5433ad2e68ca2909917698ce00100e345
Neko <neko@ayaka.moe>
2026-04-10 03:50:35 +0800

    fix(ui): incorrect round and default shape of button

--- packages/ui/src/components/misc/button.vue
+++ packages/ui/src/components/misc/button.vue
@@ -30,6 +30,7 @@ const props = withDefaults(defineProps<ButtonProps>(), {
   disabled: false,
   loading: false,
   size: 'md',
+  shape: 'pill',
   theme: 'default',
   block: false,
 })
@@ -45,7 +46,7 @@ const variantClasses: Record<ButtonVariant, Record<ButtonTheme, {
   'primary': {
     default: {
       default: [
-        'rounded-xl',
+        'rounded-lg',
         'backdrop-blur-md',
         'bg-primary-500/15 hover:bg-primary-500/20 active:bg-primary-500/30 dark:bg-primary-700/30 dark:hover:bg-primary-700/40 dark:active:bg-primary-700/30',
         'focus:ring-primary-300/60 dark:focus:ring-primary-600/30',
@@ -58,7 +59,7 @@ const variantClasses: Record<ButtonVariant, Record<ButtonTheme, {
   'secondary': {
     default: {
       default: [
-        'rounded-xl',
+        'rounded-lg',
         'backdrop-blur-md',
         'bg-neutral-100/55 hover:bg-neutral-400/20 active:bg-neutral-400/30 dark:bg-neutral-700/60 dark:hover:bg-neutral-700/80 dark:active:bg-neutral-700/60',
         'focus:ring-neutral-300/30 dark:focus:ring-neutral-600/60 dark:focus:ring-neutral-600/30',
@@ -71,7 +72,7 @@ const variantClasses: Record<ButtonVariant, Record<ButtonTheme, {
   'secondary-muted': {
     default: {
       default: [
-        'rounded-xl',
+        'rounded-lg',
         'backdrop-blur-md',
         'hover:bg-neutral-50/50 active:bg-neutral-50/90 hover:dark:bg-neutral-800/50 active:dark:bg-neutral-800/90',
         'border-2 border-solid border-neutral-100/60 dark:border-neutral-800/30',
@@ -84,7 +85,7 @@ const variantClasses: Record<ButtonVariant, Record<ButtonTheme, {
   'danger': {
     default: {
       default: [
-        'rounded-xl',
+        'rounded-lg',
         'backdrop-blur-md',
         'bg-red-500/15 hover:bg-red-500/20 active:bg-red-500/30 dark:bg-red-700/30 dark:hover:bg-red-700/40 dark:active:bg-red-700/30',
         'focus:ring-2 focus:ring-red-300/30 dark:focus:ring-red-600/60 dark:focus:ring-red-600/30',
@@ -96,7 +97,7 @@ const variantClasses: Record<ButtonVariant, Record<ButtonTheme, {
   'caution': {
     default: {
       default: [
-        'rounded-xl',
+        'rounded-lg',
         'backdrop-blur-md',
         'bg-amber-400/20 hover:bg-amber-400/25 active:bg-amber-400/35 dark:bg-amber-500/20 dark:hover:bg-amber-500/30 dark:active:bg-amber-500/35',
         'focus:ring-2 focus:ring-amber-300/40 dark:focus:ring-amber-400/40',
@@ -151,7 +152,7 @@ const baseClasses = computed(() => {
   const theme = variant[props.theme] || variant.default
 
   return [
-    'rounded-lg font-medium outline-none',
+    'font-medium outline-none',
     'transition-all duration-200 ease-in-out',
     'disabled:cursor-not-allowed disabled:opacity-50',
     'backdrop-blur-md',
