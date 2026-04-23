33e7db3387a808e84c596879eaed30c8014e93e7
Qi <shengen.qi@hpe.com>
2026-04-23 15:41:13 +0800

    Update CI and fixed format

--- packages/stage-tamagotchi-godot/scripts/StageRoot.cs
+++ packages/stage-tamagotchi-godot/scripts/StageRoot.cs
@@ -3,14 +3,14 @@ using System;
 
 public partial class StageRoot : Node3D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		GD.Print("StageRoot ready");
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        GD.Print("StageRoot ready");
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
 }
