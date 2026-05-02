b6d5f396f3446326bd5a8b9eca6ebea569cb37ed
drHuangMHT <DrHuangMHT@outlook.com>
2026-05-02 11:06:21 +0800

    prevent state drilling, component reuse, separate monitoring and transcription

--- packages/stage-layouts/src/components/Layouts/MobileInteractiveArea.vue
+++ packages/stage-layouts/src/components/Layouts/MobileInteractiveArea.vue
@@ -132,7 +132,7 @@ async function setupAnalyzer() {
   analyzerSource.connect(analyser)
 }
 
-watch([hearingDialogOpen, enabled, stream], () => {
+watch([enabled], () => {
   setupAnalyzer()
 }, { immediate: true })
 
