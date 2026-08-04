5729350b6ccd566f67d234413293a20ed507ea78
zhifu gao <zhifu.gzf@alibaba-inc.com>
2026-08-04 15:38:23 +0800

    fix(ui): support unset button size (#2207)

--- packages/ui/src/components/misc/button.vue
+++ packages/ui/src/components/misc/button.vue
@@ -24,6 +24,7 @@ const circleSizeClasses: Record<ButtonSize, string> = {
   sm: 'h-8 w-8',
   md: 'h-10 w-10',
   lg: 'h-12 w-12',
+  unset: '',
 }
 
 const colorClasses: Record<ButtonColor, Record<ButtonVariant, string[]>> = {
