c405e11b1baa215e03be6dc52955a4ac6fcf1cf7
AdairLi2504 <AdairLi0309@outlook.com>
2026-08-16 00:14:11 +0800

    download model

--- packages/stage-pages/src/pages/settings/providers/chat/prompt-api.vue
+++ packages/stage-pages/src/pages/settings/providers/chat/prompt-api.vue
@@ -1,21 +1,26 @@
 <script setup lang="ts">
 import {
+  ProviderBasicSettings,
+  ProviderDownloadModel,
   ProviderSettingsContainer,
   ProviderSettingsLayout,
   ProviderValidationAlerts,
 } from '@proj-airi/stage-ui/components'
 import { useProviderValidation } from '@proj-airi/stage-ui/composables/use-provider-validation'
+import { checkPromptAvailability, downloadModel } from 'xsai-chromium-prompt'
 
 const providerId = 'prompt-api'
 // Define computed properties for credentials
 
 // Use the composable to get validation logic and state
 const {
+  t,
   router,
   providerMetadata,
   isValidating,
   isValid,
   validationMessage,
+  handleResetSettings,
   forceValid,
   hasManualValidators,
   isManualTesting,
@@ -23,6 +28,18 @@ const {
   manualTestMessage,
   runManualTest,
 } = useProviderValidation(providerId)
+
+async function checkAvailability() {
+  return await checkPromptAvailability()
+}
+
+async function download(onProgress: (progress: number) => void) {
+  return await downloadModel(onProgress)
+}
+
+function setVaild() {
+  isValid.value = true
+}
 </script>
 
 <template>
@@ -32,6 +49,19 @@ const {
     :on-back="() => router.back()"
   >
     <ProviderSettingsContainer>
+      <ProviderBasicSettings
+        :title="t('settings.pages.providers.common.section.basic.title')"
+        :description="t('settings.pages.providers.common.section.basic.description')"
+        :on-reset="handleResetSettings"
+      >
+        <ProviderDownloadModel
+          :label="t('settings.pages.providers.catalog.edit.config.common.fields.field.download-model.label')"
+          :description="t('settings.pages.providers.catalog.edit.config.common.fields.field.download-model.label')"
+          :check-availability="checkAvailability"
+          :download="download"
+          :set-vaild="setVaild"
+        />
+      </ProviderBasicSettings>
       <ProviderValidationAlerts
         :is-valid="isValid"
         :is-validating="isValidating"
