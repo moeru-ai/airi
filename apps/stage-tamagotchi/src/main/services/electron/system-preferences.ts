cf31585fea41e01cbe6d35ab2f28f52f18f0a1cc
Neko <neko@ayaka.moe>
2026-08-27 17:12:53 +0800

    Update system-preferences.ts

--- apps/stage-tamagotchi/src/main/services/electron/system-preferences.ts
+++ apps/stage-tamagotchi/src/main/services/electron/system-preferences.ts
@@ -9,14 +9,14 @@ import { electron } from '../../../shared/eventa'
 
 export function createSystemPreferencesService(params: { context: ReturnType<typeof createContext>['context'], window: BrowserWindow }) {
   defineInvokeHandler(params.context, electron.systemPreferences.getMediaAccessStatus, (type) => {
-    if (!type || !isLinux) {
+    if (isLinux || !type) {
       return 'not-determined'
     }
 
     return systemPreferences.getMediaAccessStatus(type[0])
   })
   defineInvokeHandler(params.context, electron.systemPreferences.askForMediaAccess, (type) => {
-    if (!type || !isLinux) {
+    if (isLinux || !type) {
       return Promise.resolve(false)
     }
 
