dfae6eaf83bc92e1ce31104955e3413cffae0ea5
Makito <i@maki.to>
2026-09-07 01:06:29 +0900

    test(stage-ui-live2d): fix tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
       'packages/server-runtime',
       'packages/server-sdk',
       'packages/stage-shared',
-      'packages/stage-ui-live2d',
+      'packages/stage-ui-live2d/vitest.node.config.ts',
       'packages/vitest-plugin-fakemic',
     ],
   },
